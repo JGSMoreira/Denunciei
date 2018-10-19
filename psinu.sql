45943a9608429d1117fc973e1ee163c8387c4f80
Victor <victorsantosamaral2014@gmail.com>
2018-10-19 17:30:48 -0400

    Quase no fim

--- psinu.sql
+++ psinu.sql
@@ -14,30 +14,30 @@ create table bo(
 
   nomeVitima_bo varchar(100) not null,
   sobrenomeVitima_bo varchar(200) not null,
-  idadeVitima_bo int not null,
+  idadeVitima_bo int,
   enderecoVitima_bo varchar(400) not null,
   telefoneVitima_bo varchar(50) not null,
-  profissaoVitima_bo varchar(100) not null,
+  profissaoVitima_bo varchar(100),
 
   nomeSuspeito_bo varchar(100),
   sobrenomeSuspeito_bo varchar(200),
   idadeSuspeito_bo int,
   enderecoSuspeito_bo varchar(400),
-  telefoneSuspeito_bo varchar(50)
+  telefoneSuspeito_bo varchar(50),
 
   dataOcorrido_bo date not null,
-  descricaoOcorrido_bo varchar(3000) not null,
+  descricaoOcorrido_bo varchar(500000) not null,
   assuntoOcorrido_bo varchar(200) not null,
 
-  cod_user int not null,
-  foreign key (cod_user) references User(cod_user)
-  on delete restrict on delete cascade
-)
+
+);
 
 create table denuncia(
   cod_den int primary key auto_increment,
   titulo_den varchar(300) not null,
-  email_den varchar(300) not null,
-  descricao_den varchar(3000) not null,
+  suspeito_den varchar (1000),
+  data_den timestamp not null,
+  descricao_den varchar(500000) not null,
+  email_den varchar(300),
   assunto_den varchar(200) not null
-)
+);
